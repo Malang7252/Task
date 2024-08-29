356911ca98fc61ee028d137cd5892bbb92bb08ea
Nomanyousaf07 <nomanyousaf712@gmail.com>
2024-08-29 21:01:08 +0500

    Add project files.

---
+++ Task.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Task.Service.Mappings;
+
+namespace Task.API.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+
+        }
+
+
+        public static void AddRepos(this IServiceCollection services)
+        {
+
+        }
+
+        public static void AddAutoMapper(this IServiceCollection services)
+        {
+            // Manually configure AutoMapper
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile()); // Register your profiles here
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper); // Register IMapper as singleton
+        }
+    }
+}
---
+++ Task.API/Program.cs
@@ -0,0 +1,36 @@
+using Task.API.Extensions;
+using Task.GenericRepository;
+using AutoMapper;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddServices(); 
+builder.Services.AddRepos(); 
+builder.Services.AddAutoMapper(); 
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
